4783cb339790890dfa48a4136b7353d87d78eec9
JeongYeonSeung <asas4654@daum.net>
2024-11-10 07:44:52 +0900

    fix: 몽고 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,9 @@ import { AppService } from "./app.service";
 import { ChatGateway } from "./chat/chat.gateway";
 import { UsersModule } from "./users/users.module";
 import { AuthModule } from "./auth/auth.module";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CassetteModule } from "./cassette/cassette.module";
-import { MongooseModule } from "@nestjs/mongoose";
 import { CommonModule } from "./common/common.module";
 
 @Module({
@@ -23,14 +22,6 @@ import { CommonModule } from "./common/common.module";
       autoLoadEntities: true,
       synchronize: true,
       logging: true,
-    }),
-    MongooseModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>("MONGO_URL"),
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }),
     }),
     UsersModule,
     AuthModule,
